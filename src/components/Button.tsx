5f170e0e58788f3257cca248b026327e25ef54fa
Joseane-Guedes <joseane_23@hotmail.com>
2021-06-21 11:42:52 -0300

    day01 - first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export function Button () {
+    const [ counter, setCounter ] = useState(0)
+
+    return (
+        <button onClick={() => setCounter(counter + 1)}>
+            { counter }
+        </button>
+    )
+}
